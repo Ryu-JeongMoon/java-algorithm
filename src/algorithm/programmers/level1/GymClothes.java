02f9342ef06d0d4923f0aef3dc2bc2abc96ea624
jm <rjm9303@gmail.com>
2021-08-08 23:32:53 +0900

    20210808 크레인 인형뽑기 게임

---
+++ src/algorithm/programmers/level1/GymClothes.java
@@ -0,0 +1,28 @@
+package algorithm.programmers.level1;
+
+public class GymClothes {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(int totalStudentCount, int[] lost, int[] reserve) {
+        int answer = totalStudentCount - lost.length;
+
+        int[] clothes = createClothes(totalStudentCount);
+
+        for (int cloth : clothes) {
+
+        }
+
+        return answer;
+    }
+
+    private int[] createClothes(int totalStudentCount) {
+        int[] clothOfStudent = new int[totalStudentCount];
+        for (int i : clothOfStudent) {
+            i = 1;
+        }
+        return clothOfStudent;
+    }
+}
--- src/algorithm/programmers/level1/MakingPrimeNumber.java
+++ src/algorithm/programmers/level1/MakingPrimeNumber.java
@@ -7,8 +7,6 @@ public class MakingPrimeNumber {
         System.out.println(solution(nums));
     }
 
-    // break 걸어주는 것만으로도 시간 단축 많이 됨!
-    // stream 으로 해결하는 방법이 있을까?
     public static int solution(int[] nums) {
         int answer = 0;
 
