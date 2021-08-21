0c7de67357a65af9c27259d7b3f0bcddcbb3b5de
jm <rjm9303@gmail.com>
2021-08-21 19:28:20 +0900

    20210821 x만큼 간격이 있는 n개의 숫자

---
+++ src/algorithm/programmers/level1/IntervalAndNumber.java
@@ -0,0 +1,15 @@
+package algorithm.programmers.level1;
+
+public class IntervalAndNumber {
+    public static void main(String[] args) {
+        System.out.println(solution(4, 3));
+    }
+
+    public static long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        for (int i = 0; i < n; i++) {
+            answer[i] = x * (i + 1);
+        }
+        return answer;
+    }
+}
