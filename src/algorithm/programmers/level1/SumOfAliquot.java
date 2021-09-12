d6ee1489e1581bc2c1fefc6e07aed6b59d6d2803
jm <rjm9303@gmail.com>
2021-09-12 18:11:15 +0900

    20210912 약수의 합

---
+++ src/algorithm/programmers/level1/SumOfAliquot.java
@@ -0,0 +1,16 @@
+package algorithm.programmers.level1;
+
+import java.util.stream.IntStream;
+
+public class SumOfAliquot {
+
+    public static void main(String[] args) {
+        System.out.println(solution(5));
+    }
+
+    static int solution(int n) {
+        return IntStream.rangeClosed(1, n/2)
+            .filter(i -> n % i == 0)
+            .sum() + n;
+    }
+}
