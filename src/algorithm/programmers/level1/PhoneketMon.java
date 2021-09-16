67d36da58f7ef42aec634fa4bf058c770e6ed99a
jm <rjm9303@gmail.com>
2021-09-17 00:12:16 +0900

    20210917 폰켓몬

---
+++ src/algorithm/programmers/level1/PhoneketMon.java
@@ -0,0 +1,19 @@
+package algorithm.programmers.level1;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class PhoneketMon {
+
+    public static void main(String[] args) {
+        int[] nums = {3,3,3,2,2,2};
+        System.out.println(solution(nums));
+    }
+
+    static int solution(int[] nums) {
+        Set<Integer> phoneketMonSet = new HashSet<>();
+        Arrays.stream(nums).forEach(n -> phoneketMonSet.add(n));
+        return Math.min(nums.length/2, phoneketMonSet.size());
+    }
+}
