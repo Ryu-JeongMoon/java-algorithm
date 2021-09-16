fd8a0f238d9f3a32235473b2b307826809d563c2
jm <rjm9303@gmail.com>
2021-09-17 01:01:16 +0900

    20210917 3진법 뒤집기

---
+++ src/algorithm/programmers/level1/revertTernaryScale.java
@@ -0,0 +1,20 @@
+package algorithm.programmers.level1;
+
+public class revertTernaryScale {
+
+    public static void main(String[] args) {
+        System.out.println(solution(45));
+        System.out.println(solution(2));
+    }
+
+    static int solution(int n) {
+        StringBuilder sb = new StringBuilder();
+
+        while(n > 0){
+            sb.append(n % 3);
+            n /= 3;
+        }
+
+        return Integer.parseInt(sb.toString(), 3);
+    }
+}
