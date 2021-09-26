a55ee1287c561c232fb715347aa0128e9aa7b2ab
jm <rjm9303@gmail.com>
2021-09-26 17:47:21 +0900

    20210926 자릿수 더하기

---
+++ src/algorithm/programmers/level1/AddCiphers.java
@@ -0,0 +1,19 @@
+package algorithm.programmers.level1;
+
+public class AddCiphers {
+
+    public static void main(String[] args) {
+        System.out.println(solution(123));
+    }
+
+    public static int solution(int n) {
+        int answer = 0;
+
+        while(n > 0) {
+            int eachCipher = n % 10;
+            answer += eachCipher;
+            n /= 10;
+        }
+        return answer;
+    }
+}
