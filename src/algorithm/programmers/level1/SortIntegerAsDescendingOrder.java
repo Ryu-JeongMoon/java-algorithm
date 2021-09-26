0f783fc97b5b007e515195e4427b5c9aab99cb0b
jm <rjm9303@gmail.com>
2021-09-26 18:22:54 +0900

    20210926 정수 내림차순으로 배치하기

---
+++ src/algorithm/programmers/level1/SortIntegerAsDescendingOrder.java
@@ -0,0 +1,18 @@
+package algorithm.programmers.level1;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class SortIntegerAsDescendingOrder {
+
+    public static void main(String[] args) {
+        System.out.println(solution(118372));
+    }
+
+    public static long solution(long number) {
+        return Long.parseLong(Arrays.stream(String.valueOf(number).split(""))
+            .sorted(Comparator.reverseOrder())
+            .reduce(String::concat)
+            .orElseGet(() -> ""));
+    }
+}
