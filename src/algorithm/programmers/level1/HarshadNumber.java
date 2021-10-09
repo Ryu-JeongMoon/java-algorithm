dd41f09995a6bffe614b4ba54ac9f7a41b854c58
jm <rjm9303@gmail.com>
2021-10-10 01:31:21 +0900

    20211010 하샤드 수

---
+++ src/algorithm/programmers/level1/HarshadNumber.java
@@ -0,0 +1,21 @@
+package algorithm.programmers.level1;
+
+public class HarshadNumber {
+
+    public static void main(String[] args) {
+        System.out.println(solution(11));
+
+    }
+
+    public static boolean solution(int x) {
+        int sum = 0;
+        int y = x;
+
+        while (y > 0) {
+            sum += y % 10;
+            y /= 10;
+        }
+
+        return x % sum == 0;
+    }
+}
