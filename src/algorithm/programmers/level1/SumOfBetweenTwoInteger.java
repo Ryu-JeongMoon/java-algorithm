524e4ceffeaf9573ef7ceeaeed176f9c14e552d0
jm <rjm9303@gmail.com>
2021-10-12 23:48:59 +0900

    20211012 두 정수 사이의 합

---
+++ src/algorithm/programmers/level1/SumOfBetweenTwoInteger.java
@@ -0,0 +1,15 @@
+package algorithm.programmers.level1;
+
+import java.util.stream.LongStream;
+
+public class SumOfBetweenTwoInteger {
+
+    public static void main(String[] args) {
+        System.out.println(solution(3, 5));
+    }
+
+    public static long solution(int a, int b) {
+        return LongStream.rangeClosed(Math.min(a, b), Math.max(a, b))
+            .sum();
+    }
+}
