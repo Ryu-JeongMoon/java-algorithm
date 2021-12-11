881b4776d9e29f2e48af05301922b1e73afffb49
jm <rjm9303@gmail.com>
2021-12-11 15:52:38 +0900

    20211211 나머지가 1이 되는 수 찾기

---
+++ src/algorithm/programmers/level1/FindLeastOne.java
@@ -0,0 +1,20 @@
+package algorithm.programmers.level1;
+
+public class FindLeastOne {
+
+    public static void main(String[] args) {
+        System.out.println(solution(12));
+    }
+
+    public static int solution(int n) {
+        int answer = 0;
+        for (int i = 2; i < n; i++) {
+            if (n % i == 1) {
+                answer = i;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
