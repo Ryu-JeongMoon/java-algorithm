38ebda404049ac1d05f156aaa69abaa28ffa5227
jm <rjm9303@gmail.com>
2022-01-13 03:17:57 +0900

    20220113 개수 세기 리팩토링

--- src/algorithm/baekjoon/array/_10807.java
+++ src/algorithm/baekjoon/array/_10807.java
@@ -3,7 +3,6 @@ package algorithm.baekjoon.array;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 /**
  * 개수 세기
@@ -12,17 +11,13 @@ public class _10807 {
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
-
-    int n = Integer.parseInt(st.nextToken());
-    st = new StringTokenizer(br.readLine());
+    int n = Integer.parseInt(br.readLine());
+    String[] splittedNumbers = br.readLine().split(" ");
     int[] ints = new int[201];
     for (int i = 0; i < n; i++) {
-      ints[100 + Integer.parseInt(st.nextToken())]++;
+      ints[100 + Integer.parseInt(splittedNumbers[i])]++;
     }
-
-    st = new StringTokenizer(br.readLine());
-    int v = Integer.parseInt(st.nextToken());
+    int v = Integer.parseInt(br.readLine());
     System.out.println(ints[100 + v]);
   }
 }
