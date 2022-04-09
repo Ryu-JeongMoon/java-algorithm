b925fd671ecf494b531ca421bf672738cc620053
Ryu-JeongMoon <rjm9303@gmail.com>
2022-04-10 00:55:28 +0900

    220410 ??!

---
+++ src/algorithm/baekjoon/io_basic/_10926.java
@@ -0,0 +1,17 @@
+package algorithm.baekjoon.io_basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * ??!
+ */
+public class _10926 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String id = br.readLine();
+    System.out.print(id + "??!");
+  }
+}
