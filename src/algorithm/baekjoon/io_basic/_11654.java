d32700188d2c0adde1ca955ff49abc3c30b06017
Ryu-JeongMoon <rjm9303@gmail.com>
2022-04-11 00:28:16 +0900

    220411 아스키 코드

---
+++ src/algorithm/baekjoon/io_basic/_11654.java
@@ -0,0 +1,13 @@
+package algorithm.baekjoon.io_basic;
+
+import java.io.IOException;
+
+/**
+ * 아스키 코드
+ */
+public class _11654 {
+
+  public static void main(String[] args) throws IOException {
+    System.out.println(System.in.read());
+  }
+}
