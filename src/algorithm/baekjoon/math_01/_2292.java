42b317f3e00382c71f7ecc094d5e2efc594254f5
ryumodrn <rjm9303@gmail.com>
2022-09-06 22:23:58 +0900

    20220906 벌집

---
+++ src/algorithm/baekjoon/math_01/_2292.java
@@ -0,0 +1,35 @@
+package algorithm.baekjoon.math_01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 벌집
+ */
+public class _2292 {
+
+	public static void main(String[] args) throws IOException {
+		int i = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
+		if (i == 1) {
+			System.out.println(1);
+			return;
+		}
+
+		int count = 2, sum = 1;
+		while (true) {
+			int arithmeticSequence = (count - 1) * 6;
+			if (i <= sum + arithmeticSequence) {
+				System.out.println(count);
+				break;
+			} else {
+				sum += arithmeticSequence;
+				count++;
+			}
+		}
+	}
+}
+
+/*
+다시 풀어보기
+ */
