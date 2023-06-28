d5e5fe4c5f9695695496425b36288ffd2e06757c
ryumodrn <rjm9303@gmail.com>
2023-06-29 00:01:54 +0900

    20230629 소수 찾기

---
+++ src/main/java/algorithm/programmers/level2/_42839.java
@@ -0,0 +1,92 @@
+package algorithm.programmers.level2;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 소수 찾기
+ */
+public class _42839 {
+
+  public static void main(String[] args) {
+    _42839 instance = new _42839();
+    System.out.println(instance.solution("17"));
+    System.out.println(instance.solution("011"));
+    System.out.println(instance.solution("631"));
+  }
+
+  public int solution(String numbers) {
+    Set<String> strings = new HashSet<>();
+    boolean[] visited = new boolean[numbers.length()];
+    for (int i = 0; i < numbers.length(); i++) {
+      visited[i] = true;
+      String firstLetter = String.valueOf(numbers.charAt(i));
+      strings.add(firstLetter);
+      visit(numbers, strings, visited, firstLetter);
+      visited[i] = false;
+    }
+
+    Set<Integer> primes = new HashSet<>();
+    for (String s : strings) {
+      int n = Integer.parseInt(s);
+      if (!primes.contains(n) && isPrime(n)) {
+        primes.add(n);
+      }
+    }
+    return primes.size();
+  }
+
+  private void visit(String numbers, Set<String> numberSet, boolean[] visited, String current) {
+    if (current.length() >= numbers.length()) {
+      return;
+    }
+
+    for (int i = 0; i < numbers.length(); i++) {
+      if (!visited[i]) {
+        visited[i] = true;
+        String temporary = current + numbers.charAt(i);
+        numberSet.add(temporary);
+        visit(numbers, numberSet, visited, temporary);
+        visited[i] = false;
+      }
+    }
+  }
+
+  // even numbers don't need to be checked, so start from 3 and increase by 2
+  // zero and one are not prime numbers, so check it is greater than 1
+  private boolean isPrime(int n) {
+    for (int i = 3; i < (int) (Math.sqrt(n) + 1); i += 2) {
+      if (n % i == 0) {
+        return false;
+      }
+    }
+    return n > 1;
+  }
+}
+
+/*
+time complexity
+N: numbers.length
+O(2^N)
+
+critical point
+1. visited dfs
+if (!visited[i]) {
+  visited[i] = true;
+  String temporary = current + numbers.charAt(i);
+  numberSet.add(temporary);
+  visit(numbers, numberSet, visited, temporary);
+  visited[i] = false;
+}
+
+특정 번호 node 부터 시작하여 끝까지 방문 후
+다음 번호 node
+
+2. int casting
+(int) (Math.sqrt(n) + 1) vs Math.sqrt(n) + 1
+int casting -> (int) (Math.sqrt(n) + 1) == 2, 2를 스킵할 수 있도록 함
+후자는 double 형으로 계산되어 2를 스킵할 수 없어 소수가 아니라 판단됨
+
+reference
+https://programmers.co.kr/learn/courses/30/lessons/42839
+*/
