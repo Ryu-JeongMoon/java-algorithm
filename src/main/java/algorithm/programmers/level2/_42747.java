4cce177874c6e11cdd6ec4b509da449d3ad28c64
ryumodrn <rjm9303@gmail.com>
2023-07-06 17:25:23 +0900

    20230706 H-Index

---
+++ src/main/java/algorithm/programmers/level2/_42747.java
@@ -0,0 +1,62 @@
+package algorithm.programmers.level2;
+
+import java.util.Arrays;
+
+/**
+ * H-Index
+ */
+public class _42747 {
+
+  public static void main(String[] args) {
+    _42747 instance = new _42747();
+    System.out.println(instance.solution(new int[]{ 0, 0, 0 }));
+    System.out.println(instance.solution(new int[]{ 1, 3, 5, 7, 9, 11 }));
+    System.out.println(instance.solution(new int[]{ 3, 0, 6, 1, 5 }));
+    System.out.println(instance.solution(new int[]{ 0, 0, 0, 0, 0 }));
+    System.out.println(instance.solution(new int[]{ 0, 0, 0, 0, 1 }));
+    System.out.println(instance.solution(new int[]{ 9, 9, 9, 12 }));
+    System.out.println(instance.solution(new int[]{ 9, 7, 6, 2, 1 }));
+    System.out.println(instance.solution(new int[]{ 10, 8, 5, 4, 3 }));
+    System.out.println(instance.solution(new int[]{ 25, 8, 5, 3, 3 }));
+    System.out.println(instance.solution(new int[]{ 1, 1, 5, 7, 6 }));
+    System.out.println(instance.solution(new int[]{ 0 }));
+    System.out.println(instance.solution(new int[]{ 0, 0 }));
+  }
+
+  public int solution(int[] citations) {
+    Arrays.sort(citations);
+    int numberOfPapers = citations.length, answer = 0;
+    for (int i = numberOfPapers - 1; i > -1; i--) {
+      answer = Math.max(answer, Math.min(numberOfPapers - i, citations[i]));
+      if (numberOfPapers - i >= citations[i]) {
+        break;
+      }
+    }
+    return answer;
+  }
+}
+
+/*
+time complexity
+O(NlogN)
+
+critical point
+https://www.ibric.org/myboard/read.php?Board=news&id=270333
+피인용수가 논문수와 같아지거나 피인용수가 논문수보다 작아지기 시작하는 숫자가 바로 나의 h
+
+https://school.programmers.co.kr/questions/20906
+{ input: [1, 3, 5, 7, 9, 11], answer: 4 }
+{ input: [3, 0, 6, 1, 5], answer: 3 },
+{ input: [0, 0, 0, 0, 0], answer: 0 },
+{ input: [0, 0, 0, 0, 1], answer: 1 },
+{ input: [9, 9, 9, 12], answer: 4 },
+{ input: [9, 7, 6, 2, 1], answer: 3 },
+{ input: [10, 8, 5, 4, 3], answer: 4 },
+{ input: [25, 8, 5, 3, 3], answer: 3 },
+{ input: [1, 1, 5, 7, 6], answer: 3 },
+{ input: [0], answer: 0 },
+{ input: [0, 0], answer: 0 }
+
+reference
+https://programmers.co.kr/learn/courses/30/lessons/42747
+*/
