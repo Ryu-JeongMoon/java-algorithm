ba609bf49338b9f2061187a0ba7e657a24c13e37
ryumodrn <rjm9303@gmail.com>
2023-08-13 18:57:36 +0900

    20230813 CD

---
+++ src/main/java/algorithm/solvedac/silver_4/_4158.java
@@ -0,0 +1,45 @@
+package algorithm.solvedac.silver_4;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import algorithm.util.FastIO;
+
+/**
+ * CD
+ */
+public class _4158 {
+
+  public static void main(String[] args) throws IOException {
+    FastIO io = new FastIO();
+    int n = io.nextInt(), m = io.nextInt();
+    while (n != 0 && m != 0) {
+      int count = 0;
+      Set<Integer> elements = new HashSet<>(Math.max(n, m));
+      for (int i = 0; i < n + m; i++) {
+        int value = io.nextInt();
+        if (elements.contains(value)) {
+          ++count;
+        } else {
+          elements.add(value);
+        }
+      }
+      io.writeln(count);
+
+      n = io.nextInt();
+      m = io.nextInt();
+    }
+    io.flushBuffer();
+  }
+}
+
+/*
+time complexity
+
+critical point
+입력은 여러 개의 테스트 케이스!!
+
+reference
+https://www.acmicpc.net/problem/4158
+*/
---
+++ src/main/java/algorithm/util/FastIO.java
@@ -0,0 +1,163 @@
+package algorithm.util;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class FastIO {
+
+  private static final int BUFFER_SIZE = 1 << 16;
+  private final DataInputStream in;
+  private final DataOutputStream out;
+  private final byte[] inBuffer;
+  private final byte[] outBuffer;
+  private final byte[] bytebuffer;
+
+  private int inBufferPointer, bytesRead;
+  private int outBufferPointer;
+
+  public FastIO() {
+    in = new DataInputStream(System.in);
+    out = new DataOutputStream(System.out);
+    inBuffer = new byte[BUFFER_SIZE];
+    outBuffer = new byte[BUFFER_SIZE];
+    bytesRead = outBufferPointer = 0;
+    bytebuffer = new byte[10];
+  }
+
+  public String readLine() throws IOException {
+    byte[] buf = new byte[64]; // line length
+    int cnt = 0, c;
+    while ((c = read()) != -1) {
+      if (c == '\n') {
+        if (cnt != 0) {
+          break;
+        } else {
+          continue;
+        }
+      }
+      buf[cnt++] = (byte) c;
+    }
+    return new String(buf, 0, cnt);
+  }
+
+  public int nextInt() throws IOException {
+    int ret = 0;
+    byte c = read();
+    while (c <= ' ') {
+      c = read();
+    }
+
+    do {
+      ret = ret * 10 + c - '0';
+    } while ((c = read()) >= '0' && c <= '9');
+    return ret;
+  }
+
+  public long nextLong() throws IOException {
+    long ret = 0;
+    byte c = read();
+    while (c <= ' ') {
+      c = read();
+    }
+
+    boolean neg = (c == '-');
+    if (neg) {
+      c = read();
+    }
+
+    do {
+      ret = ret * 10 + c - '0';
+    } while ((c = read()) >= '0' && c <= '9');
+
+    return neg ? -ret : ret;
+  }
+
+  public double nextDouble() throws IOException {
+    double ret = 0, div = 1;
+    byte c = read();
+    while (c <= ' ') {
+      c = read();
+    }
+
+    boolean neg = (c == '-');
+    if (neg) {
+      c = read();
+    }
+
+    do {
+      ret = ret * 10 + c - '0';
+    } while ((c = read()) >= '0' && c <= '9');
+
+    if (c == '.') {
+      while ((c = read()) >= '0' && c <= '9') {
+        ret += (c - '0') / (div *= 10);
+      }
+    }
+
+    return neg ? -ret : ret;
+  }
+
+  private byte read() throws IOException {
+    if (inBufferPointer == bytesRead) {
+      fillBuffer();
+    }
+    return inBuffer[inBufferPointer++];
+  }
+
+  private void fillBuffer() throws IOException {
+    bytesRead = in.read(inBuffer, inBufferPointer = 0, BUFFER_SIZE);
+    if (bytesRead == -1) {
+      inBuffer[0] = -1;
+    }
+  }
+
+  public void writeln(int i) {
+    write(i);
+    writeBuffer((byte) '\n');
+  }
+
+  public void write(int i) {
+    if (i == 0) {
+      writeBuffer((byte) '0');
+      return;
+    }
+
+    if (i < 0) {
+      writeBuffer((byte) '-');
+      i = -i;
+    }
+
+    int index = 0;
+    while (i > 0) {
+      bytebuffer[index++] = (byte) ((i % 10) + '0');
+      i /= 10;
+    }
+
+    while (index-- > 0) {
+      writeBuffer(bytebuffer[index]);
+    }
+  }
+
+  private void writeBuffer(byte b) {
+    if (outBufferPointer == outBuffer.length) {
+      flushBuffer();
+    }
+    outBuffer[outBufferPointer++] = b;
+  }
+
+  public void flushBuffer() {
+    if (outBufferPointer != 0) {
+      try {
+        out.write(outBuffer, 0, outBufferPointer);
+      } catch (Exception ignored) {
+      }
+    }
+    outBufferPointer = 0;
+  }
+
+  public void close() throws IOException {
+    in.close();
+    out.close();
+  }
+}
