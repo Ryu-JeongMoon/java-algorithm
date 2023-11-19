7eb0625570bc5d5414ed3593af45459665fa022a
ryumodrn <rjm9303@gmail.com>
2023-11-19 20:31:13 +0900

    20231119 16진수

---
+++ src/main/java/algorithm/solvedac/bronze_2/_1550.java
@@ -0,0 +1,26 @@
+package algorithm.solvedac.bronze_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 16진수
+ */
+public class _1550 {
+
+  public static void main(String[] args) throws IOException {
+    System.out.println(Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine(), 16));
+  }
+}
+
+/*
+time complexity
+O(1)
+
+critical point
+진수 변환
+
+reference
+https://www.acmicpc.net/problem/1550
+*/
