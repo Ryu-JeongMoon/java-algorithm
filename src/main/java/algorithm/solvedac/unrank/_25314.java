84cbb7ff337306a3a65f350cc902f56c9c78df9a
ryumodrn <rjm9303@gmail.com>
2023-11-21 00:04:45 +0900

    20231121 코딩은 체육과목 입니다

---
+++ src/main/java/algorithm/solvedac/unrank/_25314.java
@@ -0,0 +1,25 @@
+package algorithm.solvedac.unrank;
+
+import static algorithm.util.SimpleIntReader.nextInt;
+
+import java.io.IOException;
+
+/**
+ * 코딩은 체육과목 입니다
+ */
+public class _25314 {
+
+  public static void main(String[] args) throws IOException {
+    System.out.println("long ".repeat(Math.max(0, nextInt() / 4)) + "int");
+  }
+}
+
+/*
+time complexity
+O(n)
+
+critical point
+
+reference
+https://www.acmicpc.net/problem/25314
+*/
