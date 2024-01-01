36c27ec0f7fd1f71a7b0ee4c2905775487d9be8c
ryumodrn <rjm9303@gmail.com>
2024-01-01 15:52:58 +0900

    20240101 단어 길이 재기

---
+++ src/main/java/algorithm/solvedac/bronze_5/_2743.java
@@ -0,0 +1,15 @@
+package algorithm.solvedac.bronze_5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 단어 길이 재기
+ */
+public class _2743 {
+
+  public static void main(String[] args) throws IOException {
+    System.out.println(new BufferedReader(new InputStreamReader(System.in)).readLine().length());
+  }
+}
