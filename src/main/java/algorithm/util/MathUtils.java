02fa857c926482b046f422ff4629dd3ac73a9a67
ryumodrn <rjm9303@gmail.com>
2024-04-25 00:19:36 +0900

    20240425 MathUtils#unorderedGcd

--- src/main/java/algorithm/util/MathUtils.java
+++ src/main/java/algorithm/util/MathUtils.java
@@ -5,4 +5,8 @@ public class MathUtils {
   public static int gcd(int bigger, int smaller) {
     return smaller == 0 ? bigger : gcd(smaller, bigger % smaller);
   }
+
+  public static int unorderedGcd(int a, int b) {
+    return gcd(Math.max(a, b), Math.min(a, b));
+  }
 }
