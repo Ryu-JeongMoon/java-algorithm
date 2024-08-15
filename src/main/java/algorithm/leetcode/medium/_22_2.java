bdbcd8d6b74e8fd1bf8607bed32f85633f1b8273
ryumodrn <rjm9303@gmail.com>
2024-08-16 00:12:09 +0900

    20240816 Generate Parentheses

--- src/main/java/algorithm/leetcode/medium/_22_2.java
+++ src/main/java/algorithm/leetcode/medium/_22_2.java
@@ -6,10 +6,10 @@ import java.util.List;
 /**
  * Generate Parentheses
  */
-public class _22 {
+public class _22_2 {
 
   public static void main(String[] args) {
-    _22 instance = new _22();
+    _22_2 instance = new _22_2();
 
     System.out.println(instance.generateParenthesis(3));
     System.out.println(instance.generateParenthesis(1));
@@ -17,29 +17,25 @@ public class _22 {
 
   public List<String> generateParenthesis(int n) {
     List<String> result = new ArrayList<>();
-    visit(0, n, new StringBuilder(), result);
+    visit(0, 0, n, new StringBuilder(), result);
     return result;
   }
 
-  private void visit(int open, int n, StringBuilder sb, List<String> result) {
-    if (open * 2 < sb.length() || sb.length() == 2 * n) {
-      return;
-    }
-
-    if (open == n) {
-      while (sb.length() != 2 * n) {
-        sb.append(')');
-      }
+  private void visit(int open, int close, int n, StringBuilder sb, List<String> result) {
+    if (sb.length() == n * 2) {
       result.add(sb.toString());
       return;
     }
 
-    visit(open + 1, n, sb.append('('), result);
-
-    while (sb.charAt(sb.length() - 1) == ')' && sb.length() > 1) {
+    if (open < n) {
+      visit(open + 1, close, n, sb.append('('), result);
       sb.deleteCharAt(sb.length() - 1);
     }
-    visit(open, n, sb.deleteCharAt(sb.length() - 1).append(')'), result);
+
+    if (open > close) {
+      visit(open, close + 1, n, sb.append(')'), result);
+      sb.deleteCharAt(sb.length() - 1);
+    }
   }
 }
 
@@ -48,9 +44,8 @@ time complexity
 O(2^N)
 
 critical point
-1 ms, Beats 86.31%
-유효하지 않은 괄호 제외하기
+0 ms, Beats 100.00%
 
 reference
 https://leetcode.com/problems/generate-parentheses/
-*/
+ */
